2ac66ae5aa09ddb8f4e9986a4e65f1f899cce30d
UFO <ufoqhmdt@gmail.com>
2022-09-04 17:31:13 +0800

    image

--- src/container/help/detail.tsx
+++ src/container/help/detail.tsx
@@ -26,6 +26,59 @@ import SkipNextIcon from '@mui/icons-material/SkipNext';
 import { useTheme } from '@mui/material/styles';
 import { Padding } from '@mui/icons-material';
 import style from '../../styles/help/detail.module.css';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Image from 'next/image';
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+  },
+];
 /**'
  * 求助者项目详情页面
  */
@@ -40,8 +93,15 @@ export function Detail() {
             sx={{ width: 439 }}
             style={{ padding: '32px' }}
             image="/pin1.webp"
-            alt="Live from space album cover"
-          />
+            alt="Live from space album cover">
+            <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+              {itemData.map(item => (
+                <ImageListItem key={item.img}>
+                  <Image src={`${item.img}?w=164&h=164&fit=crop&auto=format`} alt={item.title} loading="lazy" />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </CardMedia>
           <Box sx={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
             <CardContent sx={{ flex: '1 0 auto' }}>
               <Typography component="div" variant="h5">

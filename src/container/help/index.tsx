9a054cc602729e3cc5edf6f7ab40b012852325a2
xxoo <xxoo@gmail.com>
2022-09-04 12:34:36 +0800

    feat: tmp commit

--- src/container/help/index.tsx
+++ src/container/help/index.tsx
@@ -28,6 +28,7 @@ import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgr
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { url } from 'inspector';
+import { ProjectGrid } from '../../components/project/project-grid';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -146,87 +147,7 @@ export function ProjectHome() {
           </Tabs>
 
           <TabPanel value={value} index={0}>
-            <Grid container spacing={{ xs: 2, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-              {Array.from(Array(6)).map((_, index) => (
-                <Grid xs={4} sm={4} md={4} key={index}>
-                  <Card
-                    style={{
-                      position: 'relative',
-                      background: 'url(/pin1.webp)',
-                      color: 'white',
-                      height: '400px',
-                      backgroundSize: 'cover',
-                    }}
-                    sx={{ maxWidth: 345 }}
-                    onMouseEnter={e => {
-                      handleExpandClick(index);
-                    }}
-                    onMouseLeave={e => {
-                      handleExpandClick(index);
-                    }}>
-                    {/* <CardHeader title="重度烧伤" subheader="September 14, 2022，患者：老王，浙江安吉" /> */}
-
-                    {/* <CardMedia component="img" height="194" image="/pin1.webp" alt="Paella dish" /> */}
-
-                    <CardContent
-                      style={{
-                        background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 52.88%)',
-                        borderRadius: '12px',
-                        bottom: '20px',
-                        position: 'absolute',
-                      }}>
-                      <Typography gutterBottom variant="h5" component="div">
-                        重度烧伤
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary" style={{ color: 'white' }}>
-                        September 14, 2022，患者：老王，浙江安吉
-                      </Typography>
-                      <BorderLinearProgress variant="determinate" value={50} />
-                    </CardContent>
-                    {/* <CardActions disableSpacing>
-                    <IconButton aria-label="share">
-                      <ShareIcon />
-                    </IconButton>
-                    <ExpandMore
-                      expand={index === expandedIndex && expanded}
-                      // onClick={handleExpandClick}
-                      aria-expanded={index === expandedIndex && expanded}
-                      aria-label="show more">
-                      <ExpandMoreIcon />
-                    </ExpandMore>
-                  </CardActions> */}
-                    <Collapse
-                      in={index === expandedIndex && expanded}
-                      timeout="auto"
-                      unmountOnExit
-                      style={{ position: 'absolute', top: '0px', backgroundColor: 'white' }}>
-                      <CardContent>
-                        <Typography paragraph>Method:</Typography>
-                        <Typography paragraph>
-                          Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes.
-                        </Typography>
-                        <Typography paragraph>
-                          Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add
-                          chicken, shrimp and chorizo, and cook, stirring occasionally until lightly browned, 6 to 8
-                          minutes. Transfer shrimp to a large plate and set aside, leaving chicken and chorizo in the
-                          pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook, stirring
-                          often until thickened and fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-                          cups chicken broth; bring to a boil.
-                        </Typography>
-                        <Typography paragraph>
-                          Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook without
-                          stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to medium-low,
-                          add reserved shrimp and mussels, tucking them down into the rice, and cook again without
-                          stirring, until mussels have opened and rice is just tender, 5 to 7 minutes more. (Discard any
-                          mussels that don&apos;t open.)
-                        </Typography>
-                        <Typography>Set aside off of the heat to let rest for 10 minutes, and then serve.</Typography>
-                      </CardContent>
-                    </Collapse>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
+            <ProjectGrid />
             <Stack spacing={2}>
               <Pagination count={10} variant="outlined" shape="rounded" />
             </Stack>

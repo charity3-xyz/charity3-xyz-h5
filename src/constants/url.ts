03b204bb08a9fb86ca66478e3d9e3b7f53542029
xxoo <xxoo@gmail.com>
2022-09-08 22:08:24 +0800

    feat: url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,11 +1,11 @@
 // tmp
-// const debug = true;
-//
-// const api = debug
-//   ? `http://${global.location.hostname}:${location.port}/api`
-//   : `${location.protocol}//${location.hostname}${location.port ?? ''}/api/v1`;
+const debug = false;
 
-const api = 'http://localhost:3000/api';
+const api = debug
+  ? `http://${global.location.hostname}:${location.port}/api`
+  : `${location.protocol}//${location.hostname}${location.port ?? ''}/api/v1`;
+
+// const api = 'http://localhost:3000/api';
 
 export const Url = {
   web3Auth: `${api}/system/session/web3`,

0d859d050789df7f00630bed1824c84a610f4eb9
UFO <ufoqhmdt@gmail.com>
2022-09-09 12:50:24 +0800

    add partner

--- src/container/home/partner-section.tsx
+++ src/container/home/partner-section.tsx
@@ -1,7 +1,13 @@
 import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
-
+import './home.less';
 import banner from '../../assets/home/partner.png';
+import bitverse from '../../assets/home/partner/bitverse.png';
+import bybit from '../../assets/home/partner/bybit.png';
+import cryptophd from '../../assets/home/partner/cryptophd.jpg';
+import dtalk from '../../assets/home/partner/dtalk.png';
+import moledao from '../../assets/home/partner/moledao.jpg';
+import github from '../../assets/home/partner/github.png';
 
 export function PartnerSection() {
   return (
@@ -14,8 +20,13 @@ export function PartnerSection() {
         </Typography>
         <Typography variant="body2">{'There are so many people to help you realize your dreams'}</Typography>
       </Box>
-      <Box textAlign="center" mt={8}>
-        <img src={banner} alt="charity3" width="80%" />
+      <Box className="partner-img" textAlign="center" mt={8}>
+        <img className="partner-img-2" src={bitverse} alt="charity3" width="80%" />
+        <img className="partner-img-1" src={github} alt="charity3" width="80%" />
+        <img className="partner-img-3" src={bybit} alt="charity3" width="80%" />
+        <img className="partner-img-4" src={cryptophd} alt="charity3" width="80%" />
+        <img className="partner-img-5" src={dtalk} alt="charity3" width="80%" />
+        <img className="partner-img-6" src={moledao} alt="charity3" width="80%" />
       </Box>
     </Container>
   );

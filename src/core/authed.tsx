e88bb8037c17aedb39144ef06ef661416b247db1
xxoo <xxoo@gmail.com>
2022-09-18 18:06:17 +0800

    feat: 授权检查拦截器

---
+++ src/core/authed.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { sessionService } from '../services/session';
+import { route } from '../constants/route';
+
+/**
+ * 是否已经授权检查
+ * 如果已经授权、则方法可以继续执行
+ * 如果未授权则退出
+ */
+export function authed(...args: Array<any>): any {
+  if (args.length === 1) {
+    const target = args[0];
+    return class AuthWrapper extends Component<any, any> {
+      static options = target.options;
+
+      componentDidMount() {
+        const { navigation } = this.props;
+        const { replace } = navigation;
+        if (!sessionService.authorization) {
+          replace(route.SIGN_IN);
+        }
+      }
+
+      render() {
+        const TargetWrapperComponent = target;
+        return sessionService.user && <TargetWrapperComponent {...this.props} />;
+      }
+    };
+  } else {
+    const descriptor = args[2];
+    const oldValue = descriptor.value;
+    descriptor.value = function () {
+      if (sessionService.authorization) {
+        return oldValue.apply(this, arguments);
+      }
+      return undefined;
+    };
+  }
+}

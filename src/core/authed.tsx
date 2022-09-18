0d0abbf933309bcb74cef4820b4d22092f23fa6b
xxoo <xxoo@gmail.com>
2022-09-18 18:59:04 +0800

    fix: mobx 监听

--- src/core/authed.tsx
+++ src/core/authed.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { sessionService } from '../services/session';
 import { route } from '../constants/route';
 import { navigationServices } from '@aomi/mobx-history';
+import { observer } from 'mobx-react';
 
 /**
  * 是否已经授权检查
@@ -11,20 +12,22 @@ import { navigationServices } from '@aomi/mobx-history';
 export function authed(...args: Array<any>): any {
   if (args.length === 1) {
     const target = args[0];
-    return class AuthWrapper extends Component<any, any> {
-      static options = target.options;
+    return observer(
+      class AuthWrapper extends Component<any, any> {
+        static options = target.options;
 
-      componentDidMount() {
-        if (!sessionService.authorization) {
-          navigationServices.replace(route.SIGN_IN);
+        componentDidMount() {
+          if (!sessionService.authorization) {
+            navigationServices.replace(route.SIGN_IN);
+          }
         }
-      }
 
-      render() {
-        const TargetWrapperComponent = target;
-        return sessionService.user && <TargetWrapperComponent {...this.props} />;
-      }
-    };
+        render() {
+          const TargetWrapperComponent = target;
+          return sessionService.user && <TargetWrapperComponent {...this.props} />;
+        }
+      },
+    );
   } else {
     const descriptor = args[2];
     const oldValue = descriptor.value;

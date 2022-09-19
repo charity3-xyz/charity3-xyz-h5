ad808e461903121108a478b8d87f3716e60d801e
xxoo <xxoo@gmail.com>
2022-09-19 21:47:18 +0800

    feat: 生成接口调试配置

--- webapp.config.js
+++ webapp.config.js
@@ -5,12 +5,17 @@ module.exports = {
     host: '0.0.0.0',
     port: '3000',
     proxy: {
+      // local
       '/api/v1/*': {
         target: 'http://localhost:5050',
-        // target: 'http://charity3.xyz/',
         pathRewrite: { '^/api/v1': '' },
         changeOrigin: true,
       },
+      // prod
+      // '/api/v1/*': {
+      //   target: 'https://charity3.xyz/',
+      //   changeOrigin: true,
+      // },
     },
   },
   open: false,
